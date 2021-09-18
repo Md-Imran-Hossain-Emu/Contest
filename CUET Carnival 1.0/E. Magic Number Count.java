1af64da6ea02699569b8c0cdfe56ae9737bbaf0c
Md. Imran Hossain Emu <56190551+Md-Imran-Hossain-Emu@users.noreply.github.com>
2021-09-18 23:01:06 +0600

    Update E. Magic Number Count.java

--- CUET Carnival 1.0/E. Magic Number Count.java
+++ CUET Carnival 1.0/E. Magic Number Count.java
@@ -1,11 +1,9 @@
-//package JavaApplication6;
 import java.util.*;
 
 public class JavaApplication6 {
 
 static boolean[]Array=new boolean[100000001];
      public static void countPrimes(int n) {
-
         Array[0]=false;
         Array[1]=false;
         for(int i=2;i<=n;i++){
@@ -18,27 +16,21 @@ static boolean[]Array=new boolean[100000001];
                 }
             }
         }
-
     }
+     
     public static void main(String[] args) {
-        // TODO code application logic here
         countPrimes(100000000);
         Scanner xp = new Scanner(System.in);
         int a,b,c;
         a = xp.nextInt();
-        for(int i=1;i<=a;i++)
-        {
+        for(int i=1;i<=a;i++){
             b=xp.nextInt();
             c=xp.nextInt();
             int ct=0;
-            for(int j=b;j<=c;j++)
-            {
+            for(int j=b;j<=c;j++){
                 if(Array[j]) ct++;
             }
             System.out.println("Case "+i+": "+ct);
-
         }
-
     }
-
 }
